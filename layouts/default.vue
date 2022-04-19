701cc5b4ed7ca0cdcb088711d06332f0c263da1d
21ABBMax <max.wigstein@abbgymnasiet.se>
2022-04-19 13:15:15 +0200

    test

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="">
+    <!-- <div class="bg-bg-transparent shadow-2xl rounded-b-md">
+        <h1 class="text-center text-8xl pb-7">Journey through Idre</h1> -->
+    <NavBar />
+    <nuxt></nuxt>
+  </div>
+</template>
+
+<style>
+body {
+  background-color: rgb(42, 42, 42);
+}
+</style>
